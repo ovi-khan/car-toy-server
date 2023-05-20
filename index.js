3ce0b2b94a72a58076c31cd0bfcdc73795775617
ovi-khan <manikkhan2240@gmail.com>
2023-05-21 01:26:35 +0600

    vercal configaration added

--- index.js
+++ index.js
@@ -36,9 +36,16 @@ async function run() {
     const carCollection = client.db('carToys').collection('cars');
     const postToysCollection = client.db('carToys').collection('postToys')
 
+
+    app.get('/cars', async(req, res) => {
+      const cursor = carCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
     app.get('/cars/:text', async(req, res) => {
       console.log(req.params.text)
-      if(req.params.text == "Sports Cars" || req.params.text == "Battery Charge" || req.params.text == "Off-Road Vehicles") {
+      if(req.params.text == "Sports Cars" || req.params.text == "Truck" || req.params.text == "Off-Road Vehicles") {
 
         const result = await carCollection.find({categoryName: req.params.text}).toArray();
        return res.send(result)
@@ -46,8 +53,33 @@ async function run() {
       const result = carCollection.find({}).toArray();
       res.send(result)
     })
+    // app.get('/cars/:id', async(req, res) => {
+    //   console.log(req.params.id)
+    //   if(req.params.id == _id) {
 
-    app.post('/PostToys', async(req, res) => {
+    //     const result = await carCollection.find({categoryName: req.params.id}).toArray();
+    //    return res.send(result)
+    //   }
+    //   const result = carCollection.find({}).toArray();
+    //   res.send(result)
+    // })
+
+    // app.get('/cars/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await carCollection.findOne(query)
+    //   res.send(result)
+    // })
+
+
+    // post a toy
+
+    app.post('/postToys', async(req, res) => {
+      const result = await postToysCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/postToys', async(req, res) => {
       const newCar = req.body;
       const result = await postToysCollection.insertOne(newCar)
       res.send(result)
